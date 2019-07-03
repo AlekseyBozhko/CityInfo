87929906ddc6eb340d2284bb9674355aa8dc36ec
AlekseyBozhko <47057435+AlekseyBozhko@users.noreply.github.com>
2019-07-03 10:32:20 +0300

    Create empty project

---
+++ CityInfo/Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.Controllers
+{
+    public class CitiesController: Controller
+    {
+        [HttpGet("api/cities")]
+        public JsonResult GetCiTies()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new { id = 1, Name = "NY" },
+                new { id = 2, Name = "London" },
+            });
+        }
+    }
+}
